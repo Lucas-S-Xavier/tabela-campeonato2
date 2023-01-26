11e0b3be5058f16b2f813fa678e59ea49cceed61
Lucas Xavier <96668859+Lucas-S-Xavier@users.noreply.github.com>
2023-01-26 01:45:58 -0300

    aa

--- codigo/script.js
+++ codigo/script.js
@@ -1,19 +1,54 @@
-var primeira_rodada = document.getElementById ('primeira-rodada')
-var segunda_rodada = document.getElementById ('segunda-rodada')
-var numero_rodadas = document.getElementById ('numero-rodadas')
+var primeira_rodada = document.getElementById ('primeira-rodada');
+var segunda_rodada = document.getElementById ('segunda-rodada');
+var terceira_rodada = document.getElementById ('terceira-rodada');
+var quarta_rodada = document.getElementById('quarta-rodada')
+var rodada1 = document.getElementById('rodada1')
+var rodada2 = document.getElementById('rodada2')
+var rodada3 = document.getElementById('rodada3')
+var rodada4 = document.getElementById('rodada4')
 
-function clickDireita() {
+function clickDireitaUm() {
     if (segunda_rodada.style.display = 'none') {
         segunda_rodada.style.display = 'block'
         primeira_rodada.style.display = 'none'
-        numero_rodadas.innerHTML = '2° Rodada'
+        rodada2.innerHTML = '2° Rodada'
     }
 }
 
-function clickEsquerda() {
+function clickEsquerdaDois() {
     if (segunda_rodada.style.display = 'block') {
         segunda_rodada.style.display = 'none'
         primeira_rodada.style.display = 'block'
-        numero_rodadas.innerHTML = '1° Rodada'
+    }
+}
+
+function clickDireitaDois() {
+    if (terceira_rodada.style.display = 'none') {
+        terceira_rodada.style.display = 'block' 
+        segunda_rodada.style.display = 'none'
+        rodada3.innerHTML = '3° Rodada'
+    }
+}
+
+
+function clickEsquerdaTres() {
+    if (segunda_rodada.style.display = 'none') {
+        segunda_rodada.style.display = 'block'
+        terceira_rodada.style.display = 'none'
+    }
+} 
+
+function clickDireitaTres() {
+    if (quarta_rodada.style.display = 'none') {
+        quarta_rodada.style.display = 'block'
+        terceira_rodada.style.display = 'none'
+        rodada4.innerHTML = '4° Rodada'
+    }
+}
+
+function clickEsquerdaQuatro() {
+    if (terceira_rodada.style.display = 'none') {
+        terceira_rodada.style.display = 'block'
+        quarta_rodada.style.display = 'none'
     }
 }
